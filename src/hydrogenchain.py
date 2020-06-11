6406dcfe5d7f3d60e99d4376155ae7554bc66f08
DMARTI7 <60526813+DMARTI7@users.noreply.github.com>
2020-06-11 14:01:24 +0200

    Update hydrogenchain.py

--- src/hydrogenchain.py
+++ src/hydrogenchain.py
@@ -1,6 +1,6 @@
 """Function for building a hydrogen chain."""
 
-def create_hydrogen_chain_geometry(num_of_hatoms, bond_length):
+def create_hydrogen_chain_geometry(num_of_atoms, bond_length):
     """Create a molecular geometry for a hydrogen chain, along the x-axis
 
     Args:
@@ -12,7 +12,7 @@ def create_hydrogen_chain_geometry(num_of_hatoms, bond_length):
     """
    
     xyz_coords = []
-    for i in range(num_of_hatoms):
+    for i in range(num_of_atoms):
         xyz_coords.append({'species': 'H', 'x': i * bond_length, 'y': 0, 'z': 0})
        
     geometry = {'sites': xyz_coords}
